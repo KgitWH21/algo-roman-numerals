627b8f435e4d0ffcdce168d65cfaf90c55f46276
KgitWH21 <kwhayden11@gmail.com>
2026-06-30 12:49:06 -0700

    roman numerals complete

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,9 +1,57 @@
 function toRomanLazy(num) {
-  return "";
+
+  let output = "";
+  let romanNumeralToArabic = { 
+    I : 1,
+    V : 5,
+    X : 10,
+    L : 50,
+    C : 100,
+    D : 500,
+    M : 1000,
+  }
+
+  let romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
+  for (let symbol of romanNumeralPriorityOrder) {
+      const value = romanNumeralToArabic[symbol];
+      const times = Math.floor(num / value);
+
+      output += symbol.repeat(times);
+      num -= times * value;
+  }
+  return output;
 }
 
 function toRoman(num) {
-  return "";
+  let output = "";
+  let romanNumeralToArabic = {
+    I : 1,
+    IV : 4,
+    IX : 9,
+    V : 5,
+    X : 10,
+    XL : 40,
+    L : 50,
+    XC : 90,
+    C : 100,
+    CD : 400,
+    D : 500,
+    CM : 900,
+    M : 1000
+  }
+
+  let romanNumeralPriorityOrder = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  for (let symbol of romanNumeralPriorityOrder) {
+    const value = romanNumeralToArabic[symbol];
+    const times = Math.floor(num / value);
+
+    output += symbol.repeat(times);
+    num -= times * value;
+  }
+  return output;
+
 }
 
 module.exports = { toRoman, toRomanLazy };
